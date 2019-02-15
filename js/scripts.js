f66c952d932b47d29c16059ddd54fec342894e3e
Juan Orjuela <orjuelajuans@gmail.com>
2019-02-15 17:05:13 -0500

    Maqueta 1

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,37 @@ $(document).ready(function() {
         adaptiveHeight: true,
         arrows:false
       });
+      //Generar textura de puntos ========
+	$('.txt-puntos').each(function () {
+		var col = $(this).data("col"),
+			row = $(this).data("row"),
+			ancho = (col - 1) * 16 + 2 + "px",
+			alto = (row - 1) * 16 + 2 + "px",
+			nCol = 0,
+			nRow = 0;
+
+		$(this).css({
+			width: ancho,
+			height: alto
+		});
+
+		for (var i = 1; i <= col * row; i++) {
+
+			if (i % col == 1) {
+				nCol = 0;
+				nRow++;
+			} else {
+				nCol++;
+			}
+
+			var pTop = nRow * 16 - 16,
+				pLeft = nCol * 16;
+
+			//console.log("Top: "+ pTop + ", Left: "+ pLeft + " - " + index);
+
+			$(this).append('<i style=\"top: ' + pTop + 'px; left: ' + pLeft + 'px\"></i>');
+		}
+	});
+
+//Fin
 });
